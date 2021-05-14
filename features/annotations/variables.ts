711c4d83c39e6b37290955b582178efa8b9c2ad6
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-05-14 13:22:14 -0300

    Studying type any

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -27,3 +27,20 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Function that return 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20}
+
+// 2) When we declare a variable in one line
+// and intialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
